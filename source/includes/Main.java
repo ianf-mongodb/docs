99e6c94618ba314ff59b1034614c4aaa791dd94e
ian fogelman <ian.fogelman@mongodb.com>
2021-09-14 09:21:35 -0400

    BlueHawkDemo - test

---
+++ source/includes/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        // :snippet-start: test-block
+        System.out.println("Hello world!");
+        // :snippet-end:
+    }
+    }
